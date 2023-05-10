47989f828d60f3b6ffd2eeff08dbbad8b386c169
mbp13 <shaxobiddina20@gmail.com>
2023-05-10 15:56:49 +0500

    post detail change

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
 
+
 # Create your views here.
 
 
@@ -9,6 +10,11 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request,id):
-    post = get_object_or_404(Post,id=id, status = Post.Status.PUBLISHED)
-    return render(request,'blog/post/detail.html',{'post':post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
