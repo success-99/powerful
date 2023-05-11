0f997ec997106bcba06ebbe2c60cf9607cf015be
mbp13 <shaxobiddina20@gmail.com>
2023-05-11 19:21:54 +0500

    next page

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 2
     template_name = 'blog/post/list.html'
 
 
