2099272abcd44973a68881fc1daa44feb37af0a0
mbp13 <shaxobiddina20@gmail.com>
2023-05-12 01:11:46 +0500

    email

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_detail,post_share
+from .views import post_detail, post_share
 from .views import PostListView
 
 app_name = 'blog'
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
-    path('<int:post_id/share/',post_share,name='post_share'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -131,4 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'userpydj1@gmail.com'
-EMAIL_HOST_PASSWORD = 'wisrebrtrzuuolvf'
+EMAIL_HOST_PASSWORD = 'spbrafsernpbkgok'
