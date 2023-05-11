1373ed2f224a9764c57218aa8844a6c5881eaee0
mbp13 <shaxobiddina20@gmail.com>
2023-05-11 14:20:23 +0500

    next page

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
-
+from django.views.generic import ListView
 
 # Create your views here.
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(request, 'blog/post/list.html', {'posts': posts})
+#
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
