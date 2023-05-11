2c44494142dd842c914f0ff1a26e7922bd0a3436
mbp13 <shaxobiddina20@gmail.com>
2023-05-11 23:19:17 +0500

    email

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_detail
+from .views import post_detail,post_share
 from .views import PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:post_id/share/',post_share,name='post_share'),
 ]
