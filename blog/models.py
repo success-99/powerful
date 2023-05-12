68b117446bbfe280b4e1556a9c42cae617eb28a2
mbp13 <shaxobiddina20@gmail.com>
2023-05-12 20:54:23 +0500

    email

--- blog/models.py
+++ blog/models.py
@@ -49,14 +49,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=100)
     email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
-
 
     class Meta:
         ordering = ['created']
