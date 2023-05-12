49a092da39de3b764c797c0e66108996380ad041
mbp13 <shaxobiddina20@gmail.com>
2023-05-12 20:52:33 +0500

    email

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
 
 
 # Register your models here.
@@ -9,3 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish', 'status']
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'publish'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = []
