0920a7b9afe4726999f7ebe453df2b73e0d12a35
mbp13 <shaxobiddina20@gmail.com>
2023-05-23 01:09:51 +0500

    Authentication

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,25 @@
 from django import forms
 from .models import Comment
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Parol',widget=forms.PasswordInput)
+    password_2 = forms.CharField(label='Parolni takrorlang',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email','password','password_2']
+
+    def clean_password_2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password_2']:
+            raise forms.ValidationError("Ikkala parolingiz bir xil bo'lishi kerak! ")
+        return data['password_2']
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 class EmailPostForm(forms.Form):
