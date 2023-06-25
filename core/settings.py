93b31322e9579db35b48253857a05cc1a59140ef
mbp13 <shaxobiddina20@gmail.com>
2023-06-25 22:41:57 +0500

    message endtime

--- core/settings.py
+++ core/settings.py
@@ -142,10 +142,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'userpydj1@gmail.com'
-EMAIL_HOST_PASSWORD = 'uociblfbjmznatri'
-EMAIL_USE_SSL = False
+EEMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'shaxobiddina20@gmail.com'
+EMAIL_HOST_PASSWORD = 'whqyoaavjgiyejwj'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
