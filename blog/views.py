5d71e3b1e3024df714f4387ef7012be1fc5e9643
mbp13 <shaxobiddina20@gmail.com>
2023-06-25 23:04:08 +0500

    register error

--- blog/views.py
+++ blog/views.py
@@ -91,7 +91,8 @@ def user_register(request):
                 user_form.cleaned_data['password']
             )
             new_user.save()
-            return render(request,'account/register_done.html',{'new_user':new_user})
+            return redirect('blog:user_login')
+            # return render(request,'account/register_done.html',{'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
         return render(request,'account/register.html',{'user_form':user_form})
